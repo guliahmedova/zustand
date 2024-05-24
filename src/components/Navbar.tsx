2b7dc96a182815af5d540b5967f20dc8357e2d41
guluehmedova <guliehmedova19@gmail.com>
2024-05-25 00:31:35 +0400

    router

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
     return (
         <div className="navbar">
-            <Link to="/counter">Counter</Link>
+            <Link to="/">Counter</Link>
             <Link to="/courses">Courses</Link>
             <Link to="/users">Users</Link>
         </div>
